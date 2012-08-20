30e329e691adb41c990135f2de07daa1f2827207
Daniel Josefsson <dannejosefsson@gmail.com>
2012-08-20 10:43:51 +0200

    Fixed indentation to tabs.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- application/modules/admin/validators/TicketTypeName.php
+++ application/modules/admin/validators/TicketTypeName.php
@@ -1,40 +1,40 @@
-<?php 
+<?php
 class Admin_Validate_TicketTypeName extends Zend_Validate_Abstract
 {
-    protected $_translator;
-    
+	protected $_translator;
+
 	/**
-     * Error codes
-     * @const string
-     */
-    const ERROR = 'error';
-    
-    /**
-     * Error messages
-     * @var array
-     */
-    protected $_messageTemplates;
-        
-    public function isValid($value, $context = null)
-    {
-        $this->_translator = $this->getTranslator();
-        
-        $this->_messageTemplates = array(
-        self::ERROR => $this->_translator->translate('Please provide a ticket name').'.',
-       );
-        
-        unset($context['name']);
-        unset($context['order']);
-        unset($context['ticket_type_id']);
-        
-        foreach ($context as $data):
-            if(($value == '' || !isset($value)) && isset($data) && $data != '')
-            {
-                $this->_error(self::ERROR);
-                return false;
-            }
-        endforeach;
-        
-        return true;
-    }    
+	 * Error codes
+	 * @const string
+	 */
+	const ERROR = 'error';
+
+	/**
+	 * Error messages
+	 * @var array
+	 */
+	protected $_messageTemplates;
+
+	public function isValid($value, $context = null)
+	{
+		$this->_translator = $this->getTranslator();
+
+		$this->_messageTemplates = array(
+		self::ERROR => $this->_translator->translate('Please provide a ticket name').'.',
+		);
+
+		unset($context['name']);
+		unset($context['order']);
+		unset($context['ticket_type_id']);
+
+		foreach ($context as $data):
+		if(($value == '' || !isset($value)) && isset($data) && $data != '')
+		{
+			$this->_error(self::ERROR);
+			return false;
+		}
+		endforeach;
+
+		return true;
+	}
 }
--- application/modules/admin/validators/TicketTypeQuantity.php
+++ application/modules/admin/validators/TicketTypeQuantity.php
@@ -1,47 +1,46 @@
-<?php 
+<?php
 class Admin_Validate_TicketTypeQuantity extends Zend_Validate_Abstract
 {
-    protected $_translator;
-    
+	protected $_translator;
 	/**
-     * Error codes
-     * @const string
-     */
-    const ERROR = 'error';
-    
-    /**
-     * Error messages
-     * @var array
-     */
-    protected $_messageTemplates;
-        
-    public function isValid($value, $context = null)
-    {
-        $this->_translator = $this->getTranslator();
-        
-        $this->_messageTemplates = array(
-        self::ERROR => $this->_translator->translate('Please enter a number').'.',
-        );
-        
-        unset($context['quantity']);
-        unset($context['order']);
-        unset($context['ticket_type_id']);
+	 * Error codes
+	 * @const string
+	 */
+	const ERROR = 'error';
 
-        if(isset($value) && $value != '' && !is_numeric($value))
-        {
-            $this->_error(self::ERROR);
-            return false;
-        }
-        
-        // If any other required field is empty return false
-        foreach ($context as $data):
-            if(isset($data) && $data != '' && !is_numeric($value))
-            {
-                $this->_error(self::ERROR);
-                return false;
-            }
-        endforeach;
-        
-        return true;
-    }    
+	/**
+	 * Error messages
+	 * @var array
+	 */
+	protected $_messageTemplates;
+
+	public function isValid($value, $context = null)
+	{
+		$this->_translator = $this->getTranslator();
+
+		$this->_messageTemplates = array(
+		self::ERROR => $this->_translator->translate('Please enter a number').'.',
+		);
+
+		unset($context['quantity']);
+		unset($context['order']);
+		unset($context['ticket_type_id']);
+
+		if(isset($value) && $value != '' && !is_numeric($value))
+		{
+			$this->_error(self::ERROR);
+			return false;
+		}
+
+		// If any other required field is empty return false
+		foreach ($context as $data):
+		if(isset($data) && $data != '' && !is_numeric($value))
+		{
+			$this->_error(self::ERROR);
+			return false;
+		}
+		endforeach;
+
+		return true;
+	}
 }
