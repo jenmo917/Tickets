7802f46386fa101ef51507765bb43b97584de6e4
Daniel Josefsson <dannejosefsson@gmail.com>
2012-08-20 18:44:55 +0200

    Fixed indentation from spaces to tabs and fixed decorators so they are acceptable.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- application/library/Generic/Form/Base.php
+++ application/library/Generic/Form/Base.php
@@ -1,62 +1,85 @@
 <?php
-
+/**
+ * With inspiration from Phil Brown, this class decorates our forms as wanted.
+ * @link	http://blog.philipbrown.id.au/2009/10/overriding-zend-form-element-default-decorators-for-good/
+ * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 class Generic_Form_Base extends Zend_Form
 {
-    /** @var array Decorators to use for standard form elements */
-    // these will be applied to our text, password, select, checkbox and radio elements by default
-    public $elementDecorators = array(
-        'ViewHelper',
-        'Errors',
-        array('Description', array('tag' => 'p', 'class' => 'description')),
-        array('HtmlTag',     array('class' => 'form-div')),
-        array('Label',       array('class' => 'form-label', 'requiredSuffix' => '*'))
-    );
+	/** #@+ @access public */
+	/** @var array Decorators to use for standard form elements */
+	// these will be applied to our text, password, select, checkbox and radio elements by default
+	public $elementDecorators = array(
+		'ViewHelper',
+		'Errors',
+		array('Description', array('tag' => 'p', 'class' => 'description')),
+		array('Label',       array('class' => 'form-label', 'requiredSuffix' => ' *')),
+		array('HtmlTag',     array('tag' => 'li', 'class' => 'form-li')),
+	);
 
-    /** @var array Decorators for File input elements */
-    // these will be used for file elements
-    public $fileDecorators = array(
-        'File',
-        'Errors',
-        array('Description', array('tag' => 'p', 'class' => 'description')),
-        array('HtmlTag',     array('class' => 'form-div')),
-        array('Label',       array('class' => 'form-label', 'requiredSuffix' => '*'))
-    );
+	/** @var array Decorators for File input elements */
+	// these will be used for file elements
+	public $fileDecorators = array(
+		'File',
+		'Errors',
+		array('Description', array('tag' => 'p', 'class' => 'description')),
+		array('HtmlTag',     array('tag' => 'li', 'class' => 'form-file-li')),
+		array('Label',       array('class' => 'form-file-label', 'requiredSuffix' => ' *'))
+	);
 
-    /** @var array Decorator to use for standard for elements except do not wrap in HtmlTag */
-    // this array gets set up in the constructor
-    // this can be used if you do not want an element wrapped in a div tag at all
-    public $elementDecoratorsNoTag = array();
+	/** @var array Decorator to use for standard for elements except do not wrap in HtmlTag */
+	// this array gets set up in the constructor
+	// this can be used if you do not want an element wrapped in a div tag at all
+	public $elementDecoratorsNoTag = array();
 
-    /** @var array Decorators for button and submit elements */
-    // decorators that will be used for submit and button elements
-    public $buttonDecorators = array(
-        'ViewHelper',
-        array('HtmlTag', array('tag' => 'div', 'class' => 'form-button'))
-    );
+	/** @var array Decorators for button and submit elements */
+	// decorators that will be used for submit and button elements
+	public $buttonDecorators = array(
+		'ViewHelper',
+		array('HtmlTag', array('tag' => 'li', 'class' => 'form-button'))
+	);
+	/** #@- */
 
+	/**
+	 * Sets decorators for subforms currently in object.
+	 * Unlike setElementDecorators, there is no default variable for subforms.
+	 * Therefore, this function must be called after insertion of subforms to have any effect.
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @since	v0.1
+	 * @param	array $decorators Decorators that subforms should use.
+	 * @uses	Generic_Form_SubForm_Base::getBaseDecorators()
+	 */
+	public function setSubFormDecorators(array $decorators)
+	{
+		$subformDecorators = (empty($decorators))?Generic_Form_SubForm_Base::getBaseDecorators():$decorators;
+		$subformDecorators[] = array(array('outerWrap' => 'HtmlTag'), array('tag' => 'li', 'class' => 'form-subform-li'));
+		parent::setSubFormDecorators($subformDecorators);
+	}
 
-    public function __construct()
-    {
-        // first set up the $elementDecoratorsNoTag decorator, this is a copy of our regular element decorators, but do not get wrapped in a div tag
-        foreach($this->elementDecorators as $decorator) {
-            if (is_array($decorator) && $decorator[0] == 'HtmlTag') {
-                continue; // skip copying this value to the decorator
-            }
-            $this->elementDecoratorsNoTag[] = $decorator;
-        }
+	public function __construct($options = null)
+	{
+		//$this->addPrefixPath('Attend_Form', 'Attend/Form');
+		// first set up the $elementDecoratorsNoTag decorator, this is a copy of our regular element decorators, but do not get wrapped in a div tag
+		foreach($this->elementDecorators as $decorator) {
+			if (is_array($decorator) && $decorator[0] == 'HtmlTag') {
+				continue; // skip copying this value to the decorator
+			}
+			$this->elementDecoratorsNoTag[] = $decorator;
+		}
 
-        // set the decorator for the form itself, this wraps the <form> elements in a div tag instead of a dl tag
-        $this->setDecorators(array(
-                             'FormElements',
-                             array('HtmlTag', array('tag' => 'div', 'class' => 'form')),
-                             'Form'));
+		// set the decorator for the form itself.
+		$this->setDecorators(array(
+			array('FormElements', array('tag' => 'li', 'position' => 'prepend')),
+			array('HtmlTag', array('tag' => 'ol', 'class' => 'form-ol')),
+			'Form',
+		));
 
-        // set the default decorators to our element decorators, any elements added to the form
-        // will use these decorators
-        $this->setElementDecorators($this->elementDecorators);
+		// set the default decorators to our element decorators, any elements added to the form
+		// will use these decorators
+		$this->setElementDecorators($this->elementDecorators);
 
-        parent::__construct();
-        // parent::__construct must be called last because it calls $form->init()
-        // and anything after it is not executed
-    }
+		parent::__construct($options);
+		// parent::__construct must be called last because it calls $form->init()
+		// and anything after it is not executed
+	}
 }
--- application/library/Generic/Form/SubForm/Base.php
+++ application/library/Generic/Form/SubForm/Base.php
@@ -1,62 +1,97 @@
 <?php
-
+/**
+ * With inspiration from Phil Brown, this class decorates our subforms as wanted.
+ * @link	http://blog.philipbrown.id.au/2009/10/overriding-zend-form-element-default-decorators-for-good/
+ * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 class Generic_Form_SubForm_Base extends Zend_Form
 {
-    /** @var array Decorators to use for standard form elements */
-    // these will be applied to our text, password, select, checkbox and radio elements by default
-    public $elementDecorators = array(
-        'ViewHelper',
-        'Errors',
-        array('Description', array('tag' => 'p', 'class' => 'description')),
-        array('HtmlTag',     array('class' => 'subform-div')),
-        array('Label',       array('class' => 'subform-label', 'requiredSuffix' => '*'))
-    );
+	/** #@+ @access public*/
 
-    /** @var array Decorators for File input elements */
-    // these will be used for file elements
-    public $fileDecorators = array(
-        'File',
-        'Errors',
-        array('Description', array('tag' => 'p', 'class' => 'description')),
-        array('HtmlTag',     array('class' => 'subform-div')),
-        array('Label',       array('class' => 'subform-label', 'requiredSuffix' => '*'))
-    );
+	/** @var array Decorators to use on self and on child subforms*/
+	static public $subFormDecorators = array(
+		'FormElements',
+		array(array('innerWrap' => 'HtmlTag'), array('tag' => 'ol', 'class' => 'subform-ol')),
+		'Fieldset');
 
-    /** @var array Decorator to use for standard for elements except do not wrap in HtmlTag */
-    // this array gets set up in the constructor
-    // this can be used if you do not want an element wrapped in a div tag at all
-    public $elementDecoratorsNoTag = array();
+	/** @var array Decorators to use for standard form elements */
+	// these will be applied to our text, password, select, checkbox and radio elements by default.
+	public $elementDecorators = array(
+		'ViewHelper',
+		'Errors',
+		array('Description', array('tag' => 'p', 'class' => 'description')),
+		array('Label',       array('class' => 'subform-label', 'requiredSuffix' => ' *')),
+		array('HtmlTag',     array('tag' => 'li', 'class' => 'subform-li')),
+	);
 
-    /** @var array Decorators for button and submit elements */
-    // decorators that will be used for submit and button elements
-    public $buttonDecorators = array(
-        'ViewHelper',
-        array('HtmlTag', array('tag' => 'div', 'class' => 'subform-button'))
-    );
+	/** @var array Decorators for File input elements */
+	// these will be used for file elements, but it is not in use yet.
+	public $fileDecorators = array(
+		'File',
+		'Errors',
+		array('Description', array('tag' => 'p', 'class' => 'description')),
+		array('Label',       array('class' => 'subform-label', 'requiredSuffix' => '*')),
+		array('HtmlTag',     array('tag' => 'li', 'class' => 'subform-file-li')),
+	);
 
+	/** @var array Decorator to use for standard for elements except do not wrap in HtmlTag */
+	// this array gets set up in the constructor
+	// this can be used if you do not want an element wrapped in a div tag at all
+	public $elementDecoratorsNoTag = array();
 
-    public function __construct()
-    {
-        // first set up the $elementDecoratorsNoTag decorator, this is a copy of our regular element decorators, but do not get wrapped in a div tag
-        foreach($this->elementDecorators as $decorator) {
-            if (is_array($decorator) && $decorator[0] == 'HtmlTag') {
-                continue; // skip copying this value to the decorator
-            }
-            $this->elementDecoratorsNoTag[] = $decorator;
-        }
+	/** @var array Decorators for button and submit elements */
+	// decorators that will be used for submit and button elements
+	public $buttonDecorators = array(
+		'ViewHelper',
+		array('HtmlTag', array('tag' => 'li', 'class' => 'subform-button'))
+	);
+	/** #@- */
 
-        // set the decorator for the form itself, this wraps the <form> elements in a div tag instead of a dl tag
-        $this->setDecorators(array(
-                             'FormElements',
-                             array('HtmlTag', array('tag' => 'div', 'class' => 'subform')),
-                             'Form'));
+	public function __construct($options = null)
+	{
+		// first set up the $elementDecoratorsNoTag decorator, this is a copy of our regular element decorators, but do not get wrapped in a div tag
+		foreach($this->elementDecorators as $decorator) {
+			if (is_array($decorator) && $decorator[0] == 'HtmlTag') {
+				continue; // skip copying this value to the decorator
+			}
+			$this->elementDecoratorsNoTag[] = $decorator;
+		}
 
-        // set the default decorators to our element decorators, any elements added to the form
-        // will use these decorators
-        $this->setElementDecorators($this->elementDecorators);
+		// set the decorator for the form itself.
+		$this->setDecorators(self::getBaseDecorators());
 
-        parent::__construct();
-        // parent::__construct must be called last because it calls $form->init()
-        // and anything after it is not executed
-    }
+		// set the default decorators to our element decorators, any elements added to the form
+		// will use these decorators
+		$this->setElementDecorators($this->elementDecorators);
+
+		parent::__construct($options);
+		// parent::__construct must be called last because it calls $form->init()
+		// and anything after it is not executed
+	}
+
+	/**
+	 * Returns the standard decorators.
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @since	v0.1
+	 * @return	array Standard decoratiors.
+	 */
+	static public function getBaseDecorators()
+	{
+		return self::$subFormDecorators;
+	}
+
+	/**
+	 * Sets decorators for subforms currently in object.
+	 * Unlike setElementDecorators, there is no default variable for subforms.
+	 * Therefore, this function must be called after insertion of subforms to have any effect.
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @since	v0.1
+	 * @param	array $decorators Decorators that subforms should use.
+	 */
+	public function setSubFormDecorators(array $decorators)
+	{
+		$subformDecorators = (empty($decorators))?self::getBaseDecorators():$decorators;
+		$subformDecorators[] = array(array('outerWrap' => 'HtmlTag'), array('tag' => 'li', 'class' => 'form-subform-li'));
+		parent::setSubFormDecorators($subformDecorators);
+	}
 }
